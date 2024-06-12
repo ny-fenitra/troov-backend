cf8b44e6bfbb49646477b70625881536b7774bb9
Ny Fenitra <nyfenitra.rakoto@trente-douze.fr>
2024-06-12 08:30:26 +0300

    🚀 Init node project with typescript

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const serve = async () => {
+  try {
+    // Initialize Express app
+    const app = express();
+
+    app.use(express.json());
+
+    app.get('*', (req, res) => {
+      res.send('Hello World');
+    });
+
+    const port = process.env.APP_PORT || 3000;
+
+    app.listen(port, () => {
+      console.log(`App running in development on port ${port}`);
+    });
+  } catch (error) {
+    console.log('Error starting the server:', error);
+  }
+};
+
+serve();
